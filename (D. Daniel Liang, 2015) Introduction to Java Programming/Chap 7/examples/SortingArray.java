3e996b9a4496907faf08294c091b9d4e44a0e2eb
Curtis <Zhuangyongj@gmail.com>
2019-09-10 15:09:27 +0100

    SortingArray, add merge sort algorithm implementation

--- (D. Daniel Liang, 2015) Introduction to Java Programming/Chap 7/examples/SortingArray.java
+++ (D. Daniel Liang, 2015) Introduction to Java Programming/Chap 7/examples/SortingArray.java
@@ -5,12 +5,21 @@ import javax.swing.text.DefaultStyledDocument.ElementSpec;
 public class SortingArray {
     public static void main(String[] args) {
         int[] arr = { 3, 1, 3, 2, 5, 4, 7, 1, 2, 4, 9 };
-        // int[] arr = { 3, 1, 3, 2, 5, 9 };
+        System.out.println("Original: " + Arrays.toString(arr));
 
         // selectionSort(arr);
-        System.out.println(Arrays.toString(arr));
-        quickSort(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(arr));
+        // quickSort(arr, 0, arr.length - 1);
+        mergeSort(arr, 0, arr.length - 1);
+
+        // built in sorting: Arrays.sort() adopts Dual-Pivot Quicksort
+        // Arrays.sort(arr);
+
+        // built in sorting: Arrays.parallelSort() adopts multi-threading (may cause
+        // overhead )
+        // Arrays.parallelSort(arr);
+
+        System.out.println("Sorted: " + Arrays.toString(arr));
+
     }
 
     /**
@@ -78,6 +87,98 @@ public class SortingArray {
         }
         swap(arr, partitionIndex, pivotIndex);
         return partitionIndex;
+    }
+
+    /**
+     * Split to two parts if possible, merge them when the recursion ends.
+     * 
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void mergeSort(int[] arr, int left, int right) {
+
+        if (left < right) {
+            // split
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    /**
+     * merge the two parts.
+     * 
+     * @param arr
+     * @param left
+     * @param mid
+     * @param right
+     */
+    public static void merge(int arr[], int left, int mid, int right) {
+
+        // merge two arrays using the temp arrays.
+        int leftLen = mid - left + 1; // mid inclusive
+        int rightLen = right - mid; // mid exclusive
+
+        int[] leftArr = new int[leftLen];
+        int[] rightArr = new int[rightLen];
+
+        // load elements into the temp arrays
+        for (int x = 0; x < leftLen; x++) {
+            leftArr[x] = arr[x + left];
+        }
+
+        for (int x = 0; x < rightLen; x++) {
+            rightArr[x] = arr[x + mid + 1];
+        }
+
+        // compare and merge, leftArr pointer and rightArr pointer
+        int l = 0;
+        int r = 0;
+
+        // pointer of arr[]
+        int m = left;
+
+        while (l < leftLen && r < rightLen) {
+            int leftValue = leftArr[l];
+            int rightValue = rightArr[r];
+
+            if (leftValue < rightValue) {
+                // put the left element in the arr[mergedArrPointer] first
+                arr[m] = leftValue;
+                l++;
+                m++;
+            } else if (leftValue > rightValue) {
+                // put the right element in the arr[mergedArrPointer] first
+                arr[m] = rightValue;
+                r++;
+                m++;
+            } else {
+                // doesn't matter which first
+                arr[m] = leftValue;
+                m++;
+                arr[m] = rightValue;
+                m++;
+
+                l++;
+                r++;
+            }
+        }
+
+        // the leftArr or rightArr may be larger than the other, there may be remaining.
+        while (l < leftLen) {
+            arr[m] = leftArr[l];
+            l++;
+            m++;
+        }
+
+        while (r < rightLen) {
+            arr[m] = rightArr[r];
+            r++;
+            m++;
+        }
     }
 
     /**
