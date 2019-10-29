a7997842989d50df0d62ba05c5a39e25b2a45415
CurtisNewbie <Zhuangyongj@gmail.com>
2019-10-29 10:56:42 +0000

    Chap28: Implement UnweightedGraph
    	new file:   UnweightedGraph.java

---
+++ (D. Daniel Liang, 2015)/Chap28/examples/UnweightedGraph.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class UnweightedGraph<E> extends AbstractGraph<E> {
+
+    /** Empty UnweightedGraph */
+    public UnweightedGraph() {
+        super();
+    }
+
+    /**
+     * Construct Unweighted Graph using a vertices array and edges array
+     * 
+     * @param vertices vertices represented using array
+     * @param edges    edges represented using two-dimentional edges array
+     */
+    public UnweightedGraph(E[] vertices, int[][] edges) {
+        super(vertices, edges);
+    }
+
+    /**
+     * Construct Unweighted Graph using a vertices List and edges List
+     * 
+     * @param vertices vertices represented using List
+     * @param edges    edges represented using two-dimentional edges List
+     */
+    public UnweightedGraph(List<E> vertices, List<Edge> edges) {
+        super(vertices, edges);
+    }
+
+}
