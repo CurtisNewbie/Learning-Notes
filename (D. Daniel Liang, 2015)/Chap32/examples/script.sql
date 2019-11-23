d208ec1a4fb032ecebe2a7f795135a59e69e81de
CurtisNewbie <Zhuangyongj@gmail.com>
2019-11-23 18:41:26 +0000

    Chap32: Add sql script that creates example database
    	new file:   examples/script.sql

---
+++ (D. Daniel Liang, 2015)/Chap32/examples/script.sql
@@ -0,0 +1,36 @@
+
+
+-- Create table Course
+CREATE TABLE Course(
+    courseId CHAR(5),
+    subjectId CHAR(4) NOT NULL, 
+    courseNumber INTEGER, 
+    title VARCHAR(50) NOT NULL,
+    numOfCredits INTEGER,
+    PRIMARY KEY (courseId)
+);
+
+-- Create table Student
+CREATE TABLE Student(
+    studentId CHAR(5),
+    firstName VARCHAR(25),
+    lastName VARCHAR(25),
+    birthDate DATE,
+    street VARCHAR(25),
+    phone CHAR(11),
+    zipCode CHAR(5),
+    deptId CHAR(4),
+    PRIMARY KEY (studentId)
+);
+
+-- Create table Enrollment
+CREATE TABLE Enrollment(
+    studentId CHAR(5),
+    courseId CHAR(5),
+    dataRegistered DATE,
+    grade CHAR(1),
+    PRIMARY KEY (studentId, courseId),
+    FOREIGN KEY (studentId) REFERENCES Student (studentId),
+    FOREIGN KEY (courseId) REFERENCES Course (courseId)
+);
+
