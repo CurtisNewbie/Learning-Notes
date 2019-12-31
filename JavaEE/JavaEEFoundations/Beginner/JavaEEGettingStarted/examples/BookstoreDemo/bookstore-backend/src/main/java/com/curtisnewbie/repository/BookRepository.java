30b7d10f29d99b2df200558826122ed288735a11
CurtisNewbie <zhuangyongj@gmail.com>
2019-12-31 21:38:13 +0000

    JavaEE: Implemenet basic methods using JPQL
    - findAll() method
    - countAll() method
    
    modified:   bookstore-backend/src/main/java/com/curtisnewbie/repository/BookRepository.java

--- JavaEE/JavaEEFoundations/Beginner/JavaEEGettingStarted/examples/BookstoreDemo/bookstore-backend/src/main/java/com/curtisnewbie/repository/BookRepository.java
+++ JavaEE/JavaEEFoundations/Beginner/JavaEEGettingStarted/examples/BookstoreDemo/bookstore-backend/src/main/java/com/curtisnewbie/repository/BookRepository.java
@@ -5,6 +5,8 @@ import com.curtisnewbie.model.*;
 import javax.persistence.EntityNotFoundException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.*;
 
 
 /**
@@ -17,20 +19,34 @@ public class BookRepository {
     @PersistenceContext(unitName = "BookstorePU")
     private EntityManager em;
 
-    public Book find(Long id){
+    public Book find(Long id) {
         return em.find(Book.class, id);
     }
 
-    public Book create(Book book){
+    public Book create(Book book) {
         em.persist(book);
         return book;
     }
 
-    public void delete(Long id){
-        try{
+    public void delete(Long id) {
+        try {
             em.remove(em.getReference(Book.class, id));
-        }catch(EntityNotFoundException e){
-           e.printStackTrace();
+        } catch (EntityNotFoundException e) {
+            e.printStackTrace();
         }
+    }
+
+    public List<Book> findAll() {
+        //JPQL
+        // b is the alias of single Book
+        TypedQuery<Book> query = em.createQuery("SELECT b FROM Book b ORDER BY b.title DESC", Book.class);
+        return query.getResultList();
+    }
+
+    public Long countAll() {
+        //JPQL
+        // b is the alias of single Book
+        TypedQuery<Long> query = em.createQuery("SELECT COUNT(b) FROM Book b", Long.class);
+        return query.getSingleResult();
     }
 }
